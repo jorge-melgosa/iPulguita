d1b561c914b35d82111ec9f6c5ab4a3945ffe9b5
Jorge Melgosa de la Fuente <jorge.melgosa@anfix.com>
2015-06-18 16:07:25 +0200

    Prueba 3

--- CalculatorModel.swift
+++ CalculatorModel.swift
@@ -13,7 +13,7 @@ class CalculatorModel {
     var jamon: Int8
     var jamonSin: Int8
     var tortilla: Int8
-	var modificacionJorge2: Bool = true
+	var modificacionJorge3: Bool = true
     
     var jamonSubTotal: Double
     var jamonSinSubTotal: Double
