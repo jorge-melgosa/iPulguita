495eb1421f9ab3a467496dd3589f8c4b2d6b9267
Rebeca Fernández Nisa <rebeca.fernandez@anfix.com>
2015-06-18 14:17:10 +0200

    Cualquier cosa

--- CalculatorModel.swift
+++ CalculatorModel.swift
@@ -13,6 +13,7 @@ class CalculatorModel {
     var jamon: Int8
     var jamonSin: Int8
     var tortilla: Int8
+    var cualquierCosa:Bool = true
     
     var jamonSubTotal: Double
     var jamonSinSubTotal: Double
